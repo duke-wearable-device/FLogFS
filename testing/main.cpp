1ab54e6b278a1f0b514ed879a50d522079074adc
Jacob Lewallen <jlewallen@gmail.com>
2018-05-24 14:57:14 -0700

    Test software that just creates and writes a bunch of files.

--- testing/main.cpp
+++ testing/main.cpp
@@ -1,10 +1,12 @@
 #include <cstdint>
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
 
 #include <iostream>
 
 #include <flogfs.h>
+#include <flogfs_private.h>
 
 #include "flogfs_linux.h"
 
@@ -15,44 +17,89 @@ void flog_check(flog_result_t fr) {
     }
 }
 
-void write_file(const char *path) {
+constexpr char *Pattern = "abcdefgh";
+
+size_t write_file(const char *path) {
     if (!flogfs_check_exists(path)) {
+        size_t size = 0;
         flog_write_file_t file;
         std::cout << "Creating " << path << std::endl;
         flog_check(flogfs_open_write(&file, path));
         std::cout << "Writing " << path << std::endl;
-        for (auto i = 0; i < 128 * 3; ++i) {
-            flogfs_write(&file, (uint8_t *)"Jacob", 5);
+        // for (auto i = 0; i < 1024 * 26; ++i) {
+        while (size < 4*512*64) {
+            size += flogfs_write(&file, (uint8_t *)Pattern, strlen(Pattern));
         }
-        std::cout << "Closing " << path << std::endl;
+        if (size != file.file_size) {
+            std::cout << "Size is wrong! " << size << " != " << file.file_size << std::endl;
+        }
+        std::cout << "Closing " << path << " size=" << file.file_size << " block=" << file.block << " sector=" << file.sector << std::endl;
         flog_check(flogfs_close_write(&file));
+
+        return size;
     }
+
+    return 0;
+}
+
+void reopen_file(const char *path, size_t expected_size) {
+    flog_write_file_t file;
+    flog_check(flogfs_open_write(&file, path));
+    std::cout << "Opened " << path << " size=" << file.file_size << " block=" << file.block << " sector=" << file.sector << std::endl;
+    if (expected_size != file.file_size) {
+        std::cout << "Size is wrong! (exepcted != actual) " << expected_size << " != " << file.file_size << " diff=" << (file.file_size - expected_size) << std::endl;
+    }
+    flog_check(flogfs_close_write(&file));
+}
+
+void read_file(const char *path, size_t expected_size) {
+    flog_read_file_t file;
+    std::cout << "Reading " << path << std::endl;
+    flog_check(flogfs_open_read(&file, path));
+    if (expected_size != file.file_size) {
+        std::cout << "Size is wrong! " << expected_size << " != " << file.file_size << " diff=" << (file.file_size - expected_size) << std::endl;
+    }
+
+    while (true) {
+        uint8_t buffer[strlen(Pattern) + 1];
+        if (flogfs_read(&file, buffer, sizeof(buffer) - 1) != sizeof(buffer) - 1) {
+            break;
+        }
+
+        buffer[sizeof(buffer) - 1] = 0;
+
+        if (strcmp((const char *)buffer, Pattern) != 0) {
+            std::cout << buffer << std::endl;
+        }
+    }
+
+    flog_check(flogfs_close_read(&file));
 }
 
 int32_t main(int argc, char *argv[]) {
-    std::cout << "Starting" << std::endl;
-
     flog_check(flogfs_linux_open());
 
     flog_check(flogfs_init());
 
-    std::cout << "Formatting" << std::endl;
-
     flog_check(flogfs_format());
-
-    std::cout << "Mounting" << std::endl;
 
     flog_check(flogfs_mount());
 
-    write_file("data-1.bin");
-    write_file("data-2.bin");
-    write_file("data-3.bin");
+    auto size1 = write_file("data-1.bin");
+    auto size2 = write_file("data-2.bin");
+    auto size3 = write_file("data-3.bin");
+
+    read_file("data-1.bin", size1);
+    read_file("data-2.bin", size2);
+    read_file("data-3.bin", size3);
+
+    reopen_file("data-1.bin", size1);
+    reopen_file("data-2.bin", size2);
+    reopen_file("data-3.bin", size3);
 
     {
         flogfs_ls_iterator_t iter;
         char fname[256];
-
-        std::cout << "Listing: " << std::endl;
 
         flogfs_start_ls(&iter);
 
@@ -62,8 +109,6 @@ int32_t main(int argc, char *argv[]) {
 
         flogfs_stop_ls(&iter);
     }
-
-    std::cout << "Done" << std::endl;
 
     flog_check(flogfs_linux_close());
 
